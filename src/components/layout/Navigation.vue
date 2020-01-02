078ae718f68d475c047c0eddee7d5e55f20a6028
DevUrielMC <urimarcervantes.95@gmail.com>
2020-01-01 20:56:34 -0600

    First commit, Create project, Install Dependencies and create the Navigation and Views

---
+++ src/components/layout/Navigation.vue
@@ -0,0 +1,48 @@
+<template>
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <router-link class="navbar-item" to="/">
+      <h3 class ="title is-3">MyApp</h3>
+
+    </router-link >
+
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <router-link class="navbar-item" to="/">
+        Home
+      </router-link>
+
+      
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <router-link class="button is-primary" to="/register">
+            <strong>Register</strong>
+          </router-link>
+          <router-link class="button is-light" to="/login">
+            Log in
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,30 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Auth/Login.vue'
+import Register from '../views/Auth/Register.vue'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
+export default new Router ({
+  routes: [
   {
     path: '/',
     name: 'home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
   }
 ]
 
-const router = new VueRouter({
-  routes
 })
 
-export default router
+
